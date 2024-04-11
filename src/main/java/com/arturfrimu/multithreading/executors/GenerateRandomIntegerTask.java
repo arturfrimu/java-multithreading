da2a837f7dc38f98a7ec1f12e0ed4c30f1d143c4
Artur Frimu <artur.frimu2@gmail.com>
2024-04-11 09:20:12 +0300

    Add SingleThreadPoolDemo

---
+++ src/main/java/com/arturfrimu/multithreading/executors/GenerateRandomIntegerTask.java
@@ -0,0 +1,19 @@
+package com.arturfrimu.multithreading.executors;
+
+import java.util.Random;
+
+public class GenerateRandomIntegerTask implements Runnable {
+
+    private final Random random = new Random();
+
+    @Override
+    public void run() {
+        try {
+            Thread.sleep(100);
+            int randomInt = random.nextInt(1000) + 1;
+            System.out.println("SingleThreadPoolTask: " + randomInt);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
