cf9b7840d07e8d82c52fd4262336a4f3b9aa5df3
Artur Frimu <artur.frimu2@gmail.com>
2024-05-06 20:48:44 +0300

    Add DivideNumberDemo

---
+++ src/main/java/com/arturfrimu/multithreading/forkjoin/DivideNumberDemo.java
@@ -0,0 +1,11 @@
+package com.arturfrimu.multithreading.forkjoin;
+
+import java.util.concurrent.ForkJoinPool;
+
+public class DivideNumberDemo {
+    public static void main(String[] args) {
+        ForkJoinPool forkJoinPool = new ForkJoinPool();
+        DivideNumberAction divideNumberAction = new DivideNumberAction(1000);
+        forkJoinPool.invoke(divideNumberAction);
+    }
+}
