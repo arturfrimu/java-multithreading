74efbfc04581532a5f4bbf2898f63678c2b61743
Artur Frimu <artur.frimu2@gmail.com>
2024-05-22 19:28:54 +0300

    Add StarvationDemo

---
+++ src/main/java/com/arturfrimu/multithreading/synchronization/StarvationWorker.java
@@ -0,0 +1,20 @@
+package com.arturfrimu.multithreading.synchronization;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class StarvationWorker extends Thread {
+    private final AtomicInteger threadCount;
+
+    public StarvationWorker(AtomicInteger threadCount, String name) {
+        this.threadCount = threadCount;
+        this.setName(name);
+    }
+
+
+    @Override
+    public void run() {
+        threadCount.incrementAndGet();
+        System.out.println("Thread: " + this.getName() + ", counter " + threadCount + " thread execution starts");
+        System.out.println("Thread:" + this.getName() + " thread execution completes");
+    }
+}
